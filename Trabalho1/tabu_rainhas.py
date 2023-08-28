e5fb1220d36cbd9b7f55ac10f32cef35da025c65
Gustavo <gustavodiassilveira7@gmail.com>
2023-08-28 19:53:57 -0300

    Quase pronto

--- Trabalho1/tabu_rainhas.py
+++ Trabalho1/tabu_rainhas.py
@@ -1,113 +1,105 @@
-import random
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 
-def plot_chessboard(N_queens):
-    chessboard = np.zeros((N_queens, N_queens))
-
-    for i in range(N_queens):
-        for j in range(N_queens):
-            if (i + j) % 2 == 0:
-                chessboard[i, j] = 1
-
-    plt.imshow(chessboard, cmap="gray")
-    plt.xticks([])
-    plt.yticks([])
-    plt.title(f"Tabuleiro ({N_queens}x{N_queens})")
-    plt.show()
-
-def no_conflito(linha, coluna, solution):
-    return all(solution[row]       != coluna         and
-               solution[row] + row != coluna + linha and
-                solution[row] - row != coluna - linha
-               for row in range(linha))
-
-
-
-# Primeiro passo do algoritmo
-# gerando um vetor aleatório e garantindo que nenhuma 
-# rainha está na mesma coluna 
-def gerar_Solucao_Inicial(N_queens):
-    solucao_Incial = random.sample(range(1, 1 + N_queens ), N_queens )
+def gerar_Solucao_Inicial(n):
+    solucao_Incial = random.sample(range(1, 1 + n ), n )
     return solucao_Incial
 
+# Gera uma vizinhança trocando as posições de duas rainhas
+def generate_neighbors(solucao_Inicial):
+    neighbors = []
+    for i in range(len(solucao_Inicial)):
+        for j in range(i + 1, len(solucao_Inicial)):
+            neighbor = solucao_Inicial.copy()
+            neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
+            neighbors.append(neighbor)
+    return neighbors
 
+# Função para avaliar a qualidade de uma configuração
+def evaluate(board):
+    conflicts = 0
+    for i in range(len(board)):
+        for j in range(i + 1, len(board)):
+            if board[i] == board[j] or abs(board[i] - board[j]) == j - i:
+                conflicts += 1
+    return conflicts
 
-def gerar_vizinhos(solution):
-    # Criando uma variável "vizinho" e atribuindo uma lista vazia
-    # para armazenar novos vizinhos
-    vizinho = []
+# Critérios de aspiração para o problema das N-Rainhas
+def aspiration_criteria(current_conflicts, new_conflicts, tabu_list, tabu_tenure):
+    return new_conflicts < current_conflicts or (new_conflicts <= current_conflicts and tuple() not in tabu_list)
 
-    # Interação utilizando a função len que procura o maxímo de vizinhos, ou seja,
-    # a cada passada descobre um novo vizinho que resulta em uma nova solução
-    for i in range(len(solution)):
-        for j in range(len(solution)):
-            # Verificando se algum dos vizinhos possui uma melhor solução 
-            # do problema. Caso a solução seja diferente.
-            if solution[i] != j:
-                # Caso a solução seja diferente Faço uma cópia da solução
-                vizinhos = solution[:]
-                vizinhos[i] = j
-                # Estou armazenado a posição anterior na lista de vizinho
-                vizinho.append(vizinhos)
-    return vizinho
+# Implementação da busca tabu para o problema das N-Rainhas
+def tabu_search(n, max_iterations, tabu_size, tabu_tenure):
+    current_board = gerar_Solucao_Inicial(n)
+    current_conflicts = evaluate(current_board)
 
-def calc_fitness(solution):
-    conflito = 0
-    for i in range(solution):
-        for j in range(i + 1, len(solution)):
-            if solution[i] == solution[j] or abs(solution[i] - solution[j]) == j - i:
-                conflito = conflito + 1
-    return 1.0 / (1.0 + conflito)
-      
+    best_board = current_board.copy()
+    best_conflicts = current_conflicts
 
-def tabu_search(N_queens, IteMax, Freq_Proibido, solucao_Inicial,tabu_Size):
-    Solucao_Atual = gerar_Solucao_Inicial(N_queens)
-    Melhor_Solucao = solucao_Inicial[:]
-    Lista_Tabu = []
+    tabu_list = set()
 
+    for iteration in range(max_iterations):
+        best_neighbor = None
+        best_neighbor_conflicts = float('inf')
 
-    for r in range(IteMax):
-        vizinho = gerar_vizinhos(Solucao_Atual,)
-        melhor_Vizinho = None
-        melhor_vizinho_fitnees = 0.0
+        neighbors = generate_neighbors(current_board)
 
-        for vizinhos in vizinho:
-            if vizinhos not in Lista_Tabu:
-                fitnees = calc_fitness(vizinhos)
-                if fitnees > melhor_vizinho_fitnees:
-                    melhor_Vizinho = vizinhos
-                    melhor_vizinho_fitnees =  fitnees
-        
-        if melhor_Vizinho is None:
+        for neighbor in neighbors:
+            new_conflicts = evaluate(neighbor)
+
+            if aspiration_criteria(current_conflicts, new_conflicts, tabu_list, tabu_tenure) and new_conflicts < best_neighbor_conflicts:
+                best_neighbor = neighbor
+                best_neighbor_conflicts = new_conflicts
+
+        if best_neighbor is None:
             break
 
-        Lista_Tabu.append(melhor_Vizinho)
-        if len(Lista_Tabu) > tabu_size:
-            Lista_Tabu.pop(0)
-        
-        if melhor_vizinho_fitnees > calc_fitness (Melhor_Solucao):
-            Melhor_Solucao = melhor_Vizinho[:]
-        
-        Solucao_Atual = melhor_Vizinho[:]
+        current_board = best_neighbor
+        current_conflicts = best_neighbor_conflicts
 
-    return Melhor_Solucao, melhor_vizinho_fitnees
+        tabu_list.add(tuple(best_neighbor))
+        if len(tabu_list) > tabu_size:
+            tabu_list.pop()
+
+        if current_conflicts < best_conflicts:
+            best_board = current_board.copy()
+            best_conflicts = current_conflicts
+
+    return best_board
+
+# Função para plotar o tabuleiro com as rainhas
+def plot_chessboard_with_queens(board):
+    n = len(board)
+    chessboard = np.zeros((n, n))
+
+    for i, queen_col in enumerate(board):
+        chessboard[i, queen_col - 1] = 1
+
+    plt.imshow(chessboard,)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(f"N-Queens Solution ({n}x{n})")
+    plt.show()
 
 
 if __name__ == "__main__":
-    N_queens = int(input("Entre com a quantidade de rainhas."))
-    IteMax = 70
+
+    n = int(input("Entre com a quantidade de rainhas."))
+    max_iterations = 100
     tabu_size = 10
+    tabu_tenure = 5
+    print("Vetor da solução Inicial")
+    print(gerar_Solucao_Inicial(n))
 
-    # Tempo maxímo que o movimento fica na lista Tabu
-    Freq_Proibido = 5
+    vizinhanca = generate_neighbors(gerar_Solucao_Inicial(n))
 
-    # print(tabu_search(N_queens,IteMax,Freq_Proibido,tabu_size))
-    # Teste para verificar a primeira solução
-    # print(generate_Soluction(N_queens))
-    melhor_solucao, fitness = tabu_search(N_queens, IteMax, Freq_Proibido, gerar_Solucao_Inicial(N_queens), tabu_size)
-    print("Melhor solução encontrada:", melhor_solucao)
-    print("Fitness da melhor solução:", fitness)
+    for neighbors in vizinhanca:
+        print("Gerando todos os vizinhos da solução")
+        print(neighbors)
 
-    plot_chessboard(melhor_solucao)
+    best_solution = tabu_search(n, max_iterations, tabu_size, tabu_tenure)
+    print("Melhor configuração encontrada:", best_solution)
+    print("Quantidade de conflitos encontrados.", evaluate(best_solution))
+    plot_chessboard_with_queens(best_solution)
