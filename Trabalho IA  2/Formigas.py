6abb3d481ae73b5a0fb4d693492a626228f9950c
Gustavo <gustavodiassilveira7@gmail.com>
2023-10-13 23:18:27 -0300

    Up
    Esse código ainda apresenta erros. Só funciona para o primeiro grafo. Preciso ainda revisar para pelo menos pegar 3 grafos.

--- Trabalho IA  2/Formigas.py
+++ Trabalho IA  2/Formigas.py
@@ -3,7 +3,14 @@ import csv
 import random
 
 
-Numero_Formigas = 40
+'''
+Existe algum erro nesse código que ainda não encontrei,
+pois apenas o grafo do slide que encontro o valor correto.
+
+Ainda não encontrei esse erro.
+'''
+
+Numero_Formigas = 70
 Feromonio_Inicial = 0.1
 Taxa_Evaporacao = 0.01
 Max_Iteracao = 100
@@ -18,7 +25,7 @@ def criar_grafo_arquivo_csv(Arquivo_csv):
             origem, destino, custo = map(float, linha)
 
             if origem in grafo:
-                grafo[origem].append((destino, custo))
+                grafo[origem].append((destino, -custo))
             else:
                 grafo[origem] = [(destino, custo)]
             
@@ -27,6 +34,88 @@ def criar_grafo_arquivo_csv(Arquivo_csv):
             else: 
                 grafo[destino] = [(origem, custo)]
     return grafo 
+
+def Otimizacao_Colonia_formigas(grafo,Numero_Formigas,Feromonio_Inicial,Taxa_Evaporacao,Max_Iteracao):
+    Numero_Vertices = len(grafo)
+    vertices = list(grafo.keys())
+    feromonio = np.full((Numero_Vertices, Numero_Vertices), Feromonio_Inicial)
+
+    melhor_caminho = []
+    melhor_custo = float('-Inf')
+
+
+    for i in range(Max_Iteracao):
+        formigas = []
+
+        for l in range(Numero_Vertices):
+            formiga = contruir_formiga(grafo, feromonio, Numero_Vertices)
+            formigas.append(formiga)
+
+            for i in range(Numero_Vertices):
+                for j in range(Numero_Vertices):
+                    if i != j:
+                        feromonio[i][j] *= (1.0 - Taxa_Evaporacao)
+            for formiga in formigas:
+                custo_formiga = calcular_custo(formiga, grafo)
+                if custo_formiga > melhor_custo:
+                    melhor_custo = custo_formiga
+                    melhor_caminho = formiga
+                
+                deposita_feromonio(feromonio, formiga, custo_formiga)
+    return melhor_caminho, melhor_custo
+
+
+def contruir_formiga(grafo, feromonio,Numero_Vertices):
+    inicio = random.choice(list(grafo.keys()))
+    caminho_nao_visitado = set(list(grafo.keys()))
+    caminho_nao_visitado.remove(inicio)
+    caminho = [inicio]
+
+    while caminho_nao_visitado: 
+        proximo =  escolhe_vertice(grafo,feromonio, caminho[-1], caminho_nao_visitado)
+        caminho.append(proximo)
+        caminho_nao_visitado.remove(proximo)
+    return caminho
+
+def escolhe_vertice(grafo, feromonio, origem, caminho_nao_visitado):
+    Lista_Probabilidade = []
+
+    total = 0.0
+
+    for destino in caminho_nao_visitado:
+        if origem in feromonio and destino in feromonio[origem] and origem in grafo and destino in grafo[origem]:
+            prob = feromonio[origem][destino] / grafo[origem][destino]
+            Lista_Probabilidade.append((destino, prob))
+            total = total + prob
+
+    if not Lista_Probabilidade:
+        # Se não houver probabilidade válida, escolha um vértice aleatório entre os não visitados
+        escolhido = random.choice(list(caminho_nao_visitado))
+    else:
+        Lista_Probabilidade = [(destino, prob / total) for destino, prob in Lista_Probabilidade]
+        escolhido = random.choices(Lista_Probabilidade, k=1)[0][0]
+
+    return escolhido
+
+
+def calcular_custo(formiga, grafo):
+    custo = 0.0
+    for i in range(len(formiga) - 1):
+        origem = formiga[i]
+        destino = formiga[i + 1]
+        arestas = grafo[origem]
+        for (vertice, custo_aresta) in arestas:
+            if vertice == destino:
+                custo += custo_aresta
+    return custo
+
+
+def deposita_feromonio(feromonio, formiga, custo_formiga):
+    if custo_formiga > 0:
+        for i in range(len(formiga) - 1):
+            origem = int(formiga[i]) % len(feromonio)
+            destino = int(formiga[i + 1]) % len(feromonio)
+            feromonio[origem][destino] += 1.0 / custo_formiga
 
 def main():
     print("=============================================")
@@ -41,20 +130,32 @@ def main():
 
     if resposta == 1:
         ArquivoExe =  'exemplo_slides.csv'
-        grafo = criar_grafo_arquivo_csv(ArquivoExe)
-        print(grafo)
+        Armazena_Grafo = criar_grafo_arquivo_csv(ArquivoExe)
+        melhor_caminho , melhor_custo = Otimizacao_Colonia_formigas(Armazena_Grafo, Numero_Formigas, Feromonio_Inicial, Taxa_Evaporacao, Max_Iteracao)
+        print("Melhora caminho encontrado", melhor_caminho)
+        print("Melhor custo encontrado:", melhor_custo)
+   
     elif resposta == 2:
         Arquivo1 = 'grafo1.csv'
-        grafo1 = criar_grafo_arquivo_csv(Arquivo1)
-       
+        Armazena_Grafo1 = criar_grafo_arquivo_csv(Arquivo1)
+        melhor_caminho , melhor_custo = Otimizacao_Colonia_formigas(Armazena_Grafo1, Numero_Formigas, Feromonio_Inicial, Taxa_Evaporacao, Max_Iteracao)
+        print("Melhora caminho encontrado", melhor_caminho)
+        print("Melhor custo encontrado:", melhor_custo)
+    
+    
     elif resposta == 3:
         Arquivo2 = 'grafo2.csv'
-        grafo2 = criar_grafo_arquivo_csv(Arquivo2)
-
+        Armazena_Grafo2 = criar_grafo_arquivo_csv(Arquivo2)
+        melhor_caminho , melhor_custo = Otimizacao_Colonia_formigas(Armazena_Grafo2, Numero_Formigas, Feromonio_Inicial, Taxa_Evaporacao, Max_Iteracao)
+        print("Melhora caminho encontrado", melhor_caminho)
+        print("Melhor custo encontrado:", melhor_custo)
+    
     elif resposta == 4:
         Arquivo3 = 'grafo3.csv'
-        grafo2 = criar_grafo_arquivo_csv(Arquivo3)
-
+        Armazena_Grafo3 = criar_grafo_arquivo_csv(Arquivo3)
+        melhor_caminho , melhor_custo = Otimizacao_Colonia_formigas(Armazena_Grafo3, Numero_Formigas, Feromonio_Inicial, Taxa_Evaporacao, Max_Iteracao)
+        print("Melhora caminho encontrado", melhor_caminho)
+        print("Melhor custo encontrado:", melhor_custo)
     else:
         print("Ops! Algo de errado aconteceu.")
         main()
