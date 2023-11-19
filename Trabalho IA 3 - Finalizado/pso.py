b5a044c946ba5768bd041acbd5e7495403e3952d
Gustavo <gustavodiassilveira7@gmail.com>
2023-11-19 19:02:11 -0300

    V1.1

--- Trabalho IA 3 - Finalizado/pso.py
+++ Trabalho IA 3 - Finalizado/pso.py
@@ -9,77 +9,90 @@ import matplotlib.pyplot as plt
 import random
 from mpl_toolkits.mplot3d import Axes3D
 
-def f_sphere(x):
-    return np.sum(np.square(x))
+def function_sphere(x):
+    return np.sum((x**2))
 
+
+# Função Rastringin
 def rastringin_function(x):
     d = len(x)
     aux = np.sum(x**2 - 10 * np.cos(2 * np.pi * x))
     y = 10 * d + aux
     return y
 
-def f_rosenbrock(x):
+# Função de Rosembrock
+def function_rosenbrock(x):
     d = len(x)
     xi = x[0:(d-1)]
     xnext = x[1:d]
     
     sum_value = np.sum(100 * (xnext - xi**2)**2 + (xi - 1)**2)
-    
-    return sum_value
+    y = sum_value
+    return y
+
+
 # Parametrização do algoritmo
-w = 0.5
+
+# Constante de Inercia 
+w = 0.5 
+# Ponderação do individuo (congnitiva)
 c1 = 1.5
+# Ponderação global (social)
 c2 = 0.2
 
 # Parametrização de todo o experimento 
-Dimenssao_particula = 2
+
+# Quantidade dimensões do problema abordado
+Dimenssao_particula = 5
+# Função Objetivo
 function_Fitness = rastringin_function
+# Maxímo de iterações possíveis
 IteMax = 15
-Qtd_particulas = 100
+# Quantidade de particulas 
+Qtd_particulas = 200
+# Limite inferior do espaço de busca 
 limite_Inferior = -5.12
+# Limite superior do espaço de busca 
 limite_Superior = 5.12
 
+# Exibição dos graficos
 Plot_Grafico = True
 
-# Aqui deve gerar uma matriz de 200 posições, pois 100 particulas vezes a dimenssão 
-# igual a 2 é 200. Mas ainda não estou conseguindo.
-# Devo procurar uma função em python que seja parecidade com a função runif do R
-
-# gerando o vetor de velocidades 
-# Mesmo problema da matriz anterior.
 
 Matriz_Particulas = np.random.uniform(limite_Inferior, limite_Superior, size=(Qtd_particulas, Dimenssao_particula))
-velocidades = np.random.uniform(0, 1, size=(Qtd_particulas, Dimenssao_particula))
+velocidades = np.random.uniform(0,1,size=(Qtd_particulas, Dimenssao_particula))
 
-# Variável Fitness recebe um vetor de comprimento 100
-fitness = np.full(Qtd_particulas,np.nan)
+# Avalia a qualidade de cada particula com a função objetivo
+fitness = np.full(Qtd_particulas, np.nan)
 for i in range(1,Qtd_particulas):
     fitness[i] = function_Fitness(Matriz_Particulas[i,:])
 
+# Definindo PBEST de cada particula
 pbest = Matriz_Particulas
 
 
 # Identificando o GBEST de todas as particulas
 indicegbest = np.where(fitness == np.nanmin(fitness))[0][0]
-gbest = Matriz_Particulas[indicegbest,]
+gbest = Matriz_Particulas[indicegbest,:]
 fitnessgbest  = function_Fitness(Matriz_Particulas[indicegbest,:])
 
 # vetor para armazenar a fitness media e a fitness do gbest a cada iteração
-fitness_Media = np.full(IteMax,np.nan)
-fitnessgbbestiter = np.full(IteMax,np.nan)
+fitness_Media = np.full(IteMax, np.nan)
+fitnessgbbestiter = np.full(IteMax, np.nan)
 
 if Plot_Grafico == True:
-  
+#   Plotando as funções
     x = np.arange(limite_Inferior, limite_Superior, 0.6)
-    y = np.arange(limite_Inferior, limite_Superior, 0.6)
-    
+    # y = np.arange(limite_Inferior, limite_Superior, 0.6)
+    y = x
     
     z = np.zeros((len(x), len(y)))
 
     for i in range(len(x)):
         for j in range(len(y)):
             z[i, j] = function_Fitness(np.array([x[i], y[j]]))
-    # Faltando fazer um uppp nesta questão.
+    
+   
     perspectiva3D = (x,y,z)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -96,7 +109,7 @@ if Plot_Grafico == True:
 # Inicialização do Algoritmo
 
 for Ite in range(1,IteMax):
-    print(Ite)
+    print("Iterações\n",Ite)
     for i in range(1, Qtd_particulas):
 
         # Calculando o vetor de velocidades
@@ -104,22 +117,33 @@ for Ite in range(1,IteMax):
         vetor_Local = c1* (pbest[i,:]- Matriz_Particulas[i,:])
         vetor_global = c2 * (gbest - Matriz_Particulas[i,:])
 
+        # Atualiza velocidade da particula i
         velocidades[i,:] = vetor_Inercia + vetor_Local + vetor_global
-
+        # Atualiza posição da particula i
         Matriz_Particulas [i, :] = Matriz_Particulas [i,:] + velocidades [i,:]
 
+        #avalia a fitness das particula i em sua nova posicao
         novafitness = function_Fitness(Matriz_Particulas[i,:])
 
+
+        #atualiza o vetor de fitness com a fitness da nova posicao
         fitness[i] = novafitness
 
-        if novafitness < fitnessgbest:
-            indicegbest = np.where(fitness == np.nanmin(fitness))[0][0]
-            gbest = Matriz_Particulas[indicegbest, :]
-            fitnessgbest = function_Fitness(Matriz_Particulas[indicegbest,:])
         
-        # Guardando a fitness média a cada iteração
-        fitness_Media[Ite] = np.mean(fitness)
-        fitnessgbbestiter[Ite] = fitnessgbest
+        # Verifica se a nova posição e melhor que a do gbest 
+        # se for, então atualiza o pbest para a particula i
+        if novafitness < function_Fitness(pbest[i, :]):
+            pbest[i, :] = Matriz_Particulas[i, :]
+
+    #atualiza o gbest com base nas novas posicoes de todas as particulas
+    if novafitness < fitnessgbest:
+        indicegbest = np.where(fitness == np.nanmin(fitness))[0][0]
+        gbest = Matriz_Particulas[indicegbest, :]
+        fitnessgbest = function_Fitness(Matriz_Particulas[indicegbest,:])
+        
+    # Guardando a fitness média a cada iteração
+    fitness_Media[Ite] = np.mean(fitness)
+    fitnessgbbestiter[Ite] = fitnessgbest
         
 
     if Plot_Grafico == True:
@@ -133,7 +157,7 @@ for Ite in range(1,IteMax):
 
 # Exibe informações da melhor partícula (último gbest)
 print(np.round(gbest, 4))
-print(np.round(fitnessgbest, 4))
+print("Fitness do GBEST.\n",np.round(fitnessgbest, 4))
 
 # Plota fitness média ao longo das iterações
 plt.plot(fitness_Media, label="Fitness média")
